bb77c439e32f03ea1917b4d5799bd3d3b4a52131
RASCALCS <63025037+kashmir1cs@users.noreply.github.com>
2021-03-30 21:59:28 +0900

    MSSQL Query

---
+++ SQLQuery3.sql
@@ -0,0 +1,13 @@
+use testdb
+
+insert into tb_user(ID,NAME,AGE,JOB)
+values ('U001','유저','20','학생')
+
+insert into tb_user(ID,NAME,AGE,JOB)
+values ('U002','유저','28','디자이너')
+insert into tb_user(ID,NAME,AGE,JOB)
+values ('U003','유저','30','프로그래머')
+insert into tb_user(ID,NAME,AGE,JOB)
+values ('U004','유저','25','요리사')
+insert into tb_user(ID,NAME,AGE,JOB)
+values ('U005','유저','32','교사')
