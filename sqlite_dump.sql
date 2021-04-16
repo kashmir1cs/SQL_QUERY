5da69626cae78eda99203f28462ed7e8dbe6b4c4
RASCALCS <63025037+kashmir1cs@users.noreply.github.com>
2021-04-17 00:40:37 +0900

    Update sqlite_dump.sql

--- sqlite_dump.sql
+++ sqlite_dump.sql
@@ -1,9 +1,9 @@
 BEGIN TRANSACTION;
 CREATE TABLE users(id INTEGER PRIMARY KEY,     username TEXT, email TEXT,     phone TEXT, website TEXT, regdate TEXT);
-INSERT INTO "users" VALUES(1,'Kim','Kim@naver.com','010-0000-0000','Kim.com','21-04-17 00:03:15');
-INSERT INTO "users" VALUES(2,'Park','Park@daum.net','010-1111-1111','Park.com','21-04-17 00:03:15');
-INSERT INTO "users" VALUES(3,'Lee','Lee@naver.com','010-2222-2222','Lee.com','21-04-17 00:03:15');
-INSERT INTO "users" VALUES(4,'Cho','Cho@daum.net','010-3333-3333','Cho.com','21-04-17 00:03:15');
-INSERT INTO "users" VALUES(5,'Yoo','Yoo@google.com','010-4444-4444','Yoo.net','21-04-17 00:03:15');
+INSERT INTO "users" VALUES(1,'K','Ki@naver.com','02-0000-0000','Ki.com','21-04-17 00:03:15');
+INSERT INTO "users" VALUES(2,'P','Pak@daum.net','031-1111-1111','Pak.com','21-04-17 00:03:15');
+INSERT INTO "users" VALUES(3,'L','Le@naver.com','041-2222-2222','Le.com','21-04-17 00:03:15');
+INSERT INTO "users" VALUES(4,'C','Co@daum.net','02-3333-3333','Co.com','21-04-17 00:03:15');
+INSERT INTO "users" VALUES(5,'Y','Y@le.com','031-4444-4444','Y.net','21-04-17 00:03:15');
 COMMIT;
 
